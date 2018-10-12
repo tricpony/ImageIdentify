5035f7362ca852474e0553d50c5a1a8adf6d8873
aarthur <tricpony@mac.com>
2018-10-12 13:34:59 -0400

    filled out implementation of ImageIdentify

--- ImageIdentify/ImageIdentify.m
+++ ImageIdentify/ImageIdentify.m
@@ -1,6 +1,12 @@
 #import "ImageIdentify.h"
 #import <CommonCrypto/CommonDigest.h>
 
+@interface ImageIdentify()
+
+@property (strong, nonatomic) NSMutableArray *assetInfo;
+
+@end
+
 @implementation ImageIdentify
 
 + (NSArray*)imagePaths
@@ -8,21 +14,24 @@
     NSBundle *bundle = [NSBundle mainBundle];
     NSArray *pngPaths = [bundle pathsForResourcesOfType:@"png" inDirectory:nil];
     NSArray *jpgPaths = [bundle pathsForResourcesOfType:@"jpg" inDirectory:nil];
+    NSDictionary *info;
     NSString *path;
     NSMutableArray *paths = [NSMutableArray arrayWithCapacity:5];
     
     for (path in pngPaths) {
-        [paths addObject:path];
+        info = @{@"rep":@"png",@"path":path};
+        [paths addObject:info];
     }
     
     for (path in jpgPaths) {
-        [paths addObject:path];
+        info = @{@"rep":@"jpg",@"path":path};
+        [paths addObject:info];
     }
     
     return paths;
 }
 
-+ (NSString *)shaForData:(NSData*)input
++ (NSString*)shaForData:(NSData*)input
 {
     uint8_t digest[CC_SHA256_DIGEST_LENGTH];
     
@@ -40,8 +49,83 @@
     return output;
 }
 
-//- (NSString*)identifyImage:(UIView*)view
-//{
-//}
++ (NSMutableArray*)assetInfo
+{
+    NSArray *imagePaths;
+    NSMutableArray *assetInfo = [NSMutableArray arrayWithCapacity:5];
+    
+    imagePaths = [self imagePaths];
+    for (NSDictionary *nextInfo in imagePaths) {
+        NSString *sha = nil;
+        NSData *imageData = nil;
+        NSString *nextPath = nextInfo[@"path"];
+        NSString *rep = nextInfo[@"rep"];
+        UIImage *image = [UIImage imageNamed:nextPath];
+        
+        if ([rep isEqualToString:@"png"]) {
+            imageData = UIImagePNGRepresentation(image);
+        }else if ([rep isEqualToString:@"jpg"]) {
+            imageData = UIImageJPEGRepresentation(image,0.9);
+        }
+            
+        sha = [self shaForData:imageData];
+        if (sha) {
+            NSDictionary *info = nil;
+            
+            info = @{@"filename":[nextPath lastPathComponent],@"image_sha":sha,@"rep":rep};
+            [assetInfo addObject:info];
+        }
+    }
+    
+    return assetInfo;
+}
+
+- (instancetype)init
+{
+    if (self = [super init]) {
+        
+        //capture info about all the images in the app bundle
+        self.assetInfo = [[self class] assetInfo];
+    }
+    
+    return self;
+}
+
+/**
+ view is expected to be a UIImageView, normally we would want to be sure of that
+ 
+ This method extracts the image from view and tries to determine if it matches any image in the app bundle
+ If there is a match, returns the name of the image, otherwise returns an empty string
+ **/
+- (NSString*)identifyImage:(UIView*)view
+{
+    UIImageView *imageView = (id)view;
+    UIImage *image = imageView.image;
+    NSDictionary *info;
+    NSData *imageDataPngRep;
+    NSData *imageDataJpgRep;
+    NSString *imageName;
+    BOOL isMatch = NO;
+    NSPredicate *q;
+    
+    imageDataPngRep = UIImagePNGRepresentation(image);
+    imageDataJpgRep = UIImageJPEGRepresentation(image, 0.9);
+    q = [NSPredicate predicateWithFormat:@"image_sha = %@", [[self class] shaForData:imageDataJpgRep]];
+    info = [[self.assetInfo filteredArrayUsingPredicate:q] lastObject];
+    if (!info) {
+        q = [NSPredicate predicateWithFormat:@"image_sha = %@", [[self class] shaForData:imageDataPngRep]];
+        info = [[self.assetInfo filteredArrayUsingPredicate:q] lastObject];
+
+        if (info) {
+            isMatch = YES;
+            imageName = info[@"filename"];
+        }
+    }else{
+        isMatch = YES;
+        imageName = info[@"filename"];
+    }
+    
+    return isMatch ? imageName : @"";
+}
 
 @end
